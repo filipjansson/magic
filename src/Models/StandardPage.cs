b4c4253c386a332e0383c8b0d2f31562a888c527
Filip <filip.jansson@outlook.com>
2020-05-15 13:17:29 +0200

    Added startup projekt based on Piranha template

---
+++ src/Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace src.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
