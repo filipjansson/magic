29bbd5abe1d56139991a2eebcf9849060bb4e7b2
Filip <filip.jansson@outlook.com>
2020-05-18 13:48:34 +0200

    Update gulpfile.js

--- src/Magic/gulpfile.js
+++ src/Magic/gulpfile.js
@@ -3,6 +3,16 @@ var gulp = require('gulp'),
     cssmin = require("gulp-cssmin")
     rename = require("gulp-rename");
 
+var js = [
+    //{
+    //    name: "magic.js",
+    //    items: [
+    //       "",
+    //       ""
+    //    ]
+    //}
+]
+
 gulp.task('min:css', function () {
     return gulp.src('assets/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
@@ -11,4 +21,27 @@ gulp.task('min:css', function () {
             suffix: ".min"
         }))
         .pipe(gulp.dest('wwwroot/assets/css'));
-    });
+});
+
+gulp.task("min:js", function(done) {
+    for (var n = 0; n < js.length; n++) {
+        gulp.src(js[n].items, { base: "." })
+            .pipe(vueCompile())
+            .pipe(concat(output + "js/" + js[n].name))
+            .pipe(gulp.dest("."))
+            .pipe(uglify().on('error', function(e) {
+                console.log(e);
+            }))
+            .pipe(rename({
+                suffix: ".min"
+            }))
+            .pipe(gulp.dest("."));
+    }
+    done();
+});
+
+//
+// Default tasks
+//
+gulp.task("build", gulp.parallel(["min:css", "min:js"]));
+gulp.task("default", gulp.series("build"));
